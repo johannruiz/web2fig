4b230cca76845d9ac774ac0505d079dc8ff1727c
johannruiz <63679587+johannruiz@users.noreply.github.com>
2026-07-02 12:37:14 -0500

    Delete desktop-electron directory

--- desktop-electron/electron/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("web2figDesktop", {
-  captureUrl(options) {
-    return ipcRenderer.invoke("web2fig:capture-url", options);
-  },
-});
